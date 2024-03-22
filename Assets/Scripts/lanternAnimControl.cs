c6f92889798eeedb61a95a0ef40fd6ed17ab39c1
Erik <erikpapp@external.mol.hu>
2024-03-22 17:18:15 +0000

    lanterntouch logic switched from player to lantern gameobject

--- Assets/Scripts/lanternAnimControl.cs
+++ Assets/Scripts/lanternAnimControl.cs
@@ -14,7 +14,6 @@ public class lanternAnimControl : MonoBehaviour
 
     private SpriteRenderer lanternSpriteRenderer;
 
-
     // Start is called before the first frame update
     void Awake()
     {
@@ -25,28 +24,38 @@ public class lanternAnimControl : MonoBehaviour
         lanternSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    public bool superDashPotential = false;
+    private bool superDashPotential;
+    public float superDashLanternCooldown = 3.0f;
+    private float _timeLanternUsed;
     // Update is called once per frame
     void Update()
     {
-        if (Physics2D.OverlapCircle(new Vector2(this.transform.position.x, this.transform.position.y), 0.1f, pm.PlayerLayer) && pm.lanternTouch && !superDashPotential)
+        if (pm._time >= _timeLanternUsed + superDashLanternCooldown)
         {
-            superDashPotential = true;
-            anim.SetBool("lanternActive", true);
-        }
-
-        else if (superDashPotential && !pm.superDashSuccess)
-        {
-            if (pm.superDashReleased)
+            lanternSpriteRenderer.enabled = true;
+            if (pm._dashing && Physics2D.OverlapCircle(new Vector2(this.transform.position.x, this.transform.position.y), 0.1f, pm.PlayerLayer) && !superDashPotential)
             {
-                lanternSpriteRenderer.enabled = false;
-                superDashPotential = false;
-                anim.SetBool("lanternActive", false);
+                pm.lanternTouch = true;
+                pm._usedSuperDash = false;
+                pm._timeHitLantern = pm._time;
+                superDashPotential = true;
+                anim.SetBool("lanternActive", true);
             }
-            else if (pm._usedSuperDash)
+
+            else if (superDashPotential && !pm.superDashSuccess)
             {
-                superDashPotential = false;
-                anim.SetBool("lanternActive", false);
+                if (pm.superDashReleased)
+                {
+                    lanternSpriteRenderer.enabled = false;
+                    superDashPotential = false;
+                    anim.SetBool("lanternActive", false);
+                    _timeLanternUsed = pm._time;
+                }
+                else if (pm._usedSuperDash)
+                {
+                    superDashPotential = false;
+                    anim.SetBool("lanternActive", false);
+                }
             }
         }
     }
